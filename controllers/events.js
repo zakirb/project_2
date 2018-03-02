5abedb80002939cbb02e4fbc3a8c393da45b7238
Zakir Butte <zakir.butte@gmailcom>
2018-03-02 12:47:54 -0800

    added zipcode to geohash conversion and search option

--- controllers/events.js
+++ controllers/events.js
@@ -4,6 +4,8 @@ var passport = require('../config/ppConfig');
 var router = express.Router();
 var request = require('request');
 var isLoggedIn = require('../middleware/isLoggedIn');
+var Geohash = require('latlon-geohash');
+var zipcodes = require('zipcodes');
 
 router.get('/', function(req, res) {
   var data;
@@ -14,9 +16,31 @@ router.get('/', function(req, res) {
 });
 
 router.get('/results', function(req, res) {
-  var eventUrl = "https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&size=20&apikey=" + process.env.TM_API_KEY + '&segmentName=music&sort=date,asc&keyword=' + req.query.keyword;
-  
-  request(eventUrl, function(error, response, body) {
+
+  var searchZip = req.query.zipcode;
+  if (searchZip.length === 5) {
+    var zipData = zipcodes.lookup(searchZip);
+    var lat = zipData.latitude;
+    var long = zipData.longitude;
+    var searchGeohash = Geohash.encode(lat, long, 9);
+  }
+
+  var qs = {
+    countryCode: 'US',
+    size: 20,
+    segmentName: 'music',
+    sort: 'date,asc',
+    apikey: process.env.TM_API_KEY,
+    keyword: req.query.keyword,
+    geoPoint: searchGeohash,
+    units: 'miles',
+    radius: req.query.radius
+  };
+
+  request({
+    url: 'https://app.ticketmaster.com/discovery/v2/events.json',
+    qs: qs
+  }, function(error, response, body) {
     if (!error && response.statusCode == 200 && (JSON.parse(body)._embedded)) {
         var dataObj = JSON.parse(body);
         // console.log(dataObj._embedded.events);
